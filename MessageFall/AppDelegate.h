9414bc6384c3a0d677db1d7e09aa00e42d785220
FFFFFFMILK <wt1992cuc@gmail.com>
2017-07-25 22:58:25 +0800

    Initial Commit

---
+++ MessageFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageFall
+//
+//  Created by Fu on 2017/7/25.
+//  Copyright © 2017年 Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageFall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageFall
+//
+//  Created by Fu on 2017/7/25.
+//  Copyright © 2017年 Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
